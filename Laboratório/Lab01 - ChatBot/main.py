96f5587fe445ef104bd8df4483153fd2e5fd28eb
jvgoverna <jv11governa2018@gmail.com>
2025-02-12 11:53:05 -0300

    Initial commit

---
+++ Laboratório/Lab01 - ChatBot/main.py
@@ -0,0 +1,22 @@
+from chatBot import ChatBot
+myChatBot = ChatBot()
+
+
+#criar o modelo
+myChatBot.createModel()
+
+
+print("Bem vindo ao Chatbot")
+
+
+pergunta = input("como posso te ajudar?")
+resposta, intencao = myChatBot.chatbot_response(pergunta)
+print(resposta + "   ["+intencao[0]['intent']+"]")
+
+
+while (intencao[0]['intent']!="despedida"):
+    pergunta = input("posso lhe ajudar com algo a mais?")
+    resposta, intencao = myChatBot.chatbot_response(pergunta)
+    print(resposta + "   [" + intencao[0]['intent'] + "]")
+
+print("Foi um prazer atendê-lo")
