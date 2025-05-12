3d9ba5743c78568199204ad7b406ee179712928d
jvgoverna <jv11governa2018@gmail.com>
2025-05-11 22:41:45 -0300

    ADD boxes names
    Co-Authored-By: FelipeOrlandoLanzara <115379350+FelipeOrlandoLanzara@users.noreply.github.com>

---
+++ ProjetoIA/controllers/pos_caixas/build/release/pos_caixas.d
@@ -0,0 +1,9 @@
+build/release/pos_caixas.o: pos_caixas.c \
+ /snap/webots/27/usr/share/webots/include/controller/c/webots/robot.h \
+ /snap/webots/27/usr/share/webots/include/controller/c/webots/types.h \
+ /snap/webots/27/usr/share/webots/include/controller/c/webots/nodes.h \
+ /snap/webots/27/usr/share/webots/include/controller/c/webots/motor.h \
+ /snap/webots/27/usr/share/webots/include/controller/c/webots/distance_sensor.h \
+ /snap/webots/27/usr/share/webots/include/controller/c/webots/led.h \
+ /snap/webots/27/usr/share/webots/include/controller/c/webots/supervisor.h \
+ /snap/webots/27/usr/share/webots/include/controller/c/webots/contact_point.h
